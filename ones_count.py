c4b56960ebb2310f60c6c8dc1dd4c3fe75a37942
zewaves <m1crowavesze@gmail.com>
2025-07-15 15:45:26 +0530

    bruh22

---
+++ ones_count.py
@@ -0,0 +1,22 @@
+m=int(input("Enter the no of rows:"))
+n=int(input("Enter the no of cols:"))               
+print(f"Enter {m*n} elements:")
+iplist=list(map(int,input().split()))
+mat=[]
+for k in range(m):
+    start=k*n
+    end=start+n
+    row=iplist[start:end]
+    mat.append(row)
+
+index=-1
+count=0
+for i in range(m):
+    rowcount=0
+    for j in range(n):
+        if mat[i][j]==1:
+            rowcount+=1
+    if rowcount>count:
+        count=rowcount
+        index=i
+print(index+1)
