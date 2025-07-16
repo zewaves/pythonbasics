78504f5f2c9314d51ea20fa370dfcd08b5457780
zewaves <m1crowavesze@gmail.com>
2025-07-16 12:29:37 +0530

    insert_after function added to single linked list

--- single_linked_list.py
+++ single_linked_list.py
@@ -32,10 +32,21 @@ class LinkedList:
         previousfront = self.head
         self.head = newnode
         newnode.next = previousfront
+    def insert_after(self, prev, data):
+        current = self.head
+        while current:
+            if current.data == prev:
+                newnode = Node(data)
+                newnode.next = current.next
+                current.next = newnode
+                return
+            current = current.next
+        print(f"{prev} not found")
 
 L1 = LinkedList()
 L1.insert_end(10)
 L1.insert_end(20)
 L1.insert_end(30)
 L1.insert_front(00)
+L1.insert_after(20, 25)
 L1.display()
